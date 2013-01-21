e206298d0640e0ccc90c394c78151e4e3f2e9210
Chris Klein <chrisklein@nomabi.com>
2013-01-20 18:24:50 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , mqtt = require('mqttjs')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+mqtt.createServer(function(client) {
+  var self = this;
+
+  if (!self.clients) self.clients = {};
+
+  client.on('connect', function(packet) {
+    client.connack({returnCode: 0});
+    client.id = packet.client;
+    self.clients[client.id] = client;
+  });
+
+  client.on('publish', function(packet) {
+    for (var k in self.clients) {
+      self.clients[k].publish({topic: packet.topic, payload: packet.payload});
+    }
+  });
+
+  client.on('subscribe', function(packet) {
+    var granted = [];
+    for (var i = 0; i < packet.subscriptions.length; i++) {
+      granted.push(packet.subscriptions[i].qos);
+    }
+
+    client.suback({granted: granted});
+  });
+
+  client.on('pingreq', function(packet) {
+    client.pingresp();
+  });
+
+  client.on('disconnect', function(packet) {
+    client.stream.end();
+  });
+
+  client.on('close', function(err) {
+    delete self.clients[client.id];
+  });
+
+  client.on('error', function(err) {
+    client.stream.end();
+    util.log('error!');
+  });
+}).listen(1883);
